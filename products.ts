5b6c84804339059499b95190e35791c6f45c6502
Amanzhan989 <74005565+Amanzhan989@users.noreply.github.com>
2021-02-24 14:42:24 +0600

    Add files via upload

---
+++ products.ts
@@ -0,0 +1,92 @@
+export const products = [
+{
+  id: 1,
+  name: '1. TCL 43S325 43 Inch 1080p Smart LED Roku TV (2019)',
+  price: 799,
+  description: 'This FHD display with IPS technology gives you brilliant visuals and unforgettable quality; with a maximum resolution of 1920 x 1080 ',
+  rating: '9/10',
+  link: "https://www.amazon.com/TCL-43S325-Inch-1080p-Smart/dp/B07G9W9ZR9/ref=sr_1_1?dchild=1&field-shipping_option-bin=3242350011&pf_rd_i=16225009011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=85a9188d-dbd5-424e-9512-339a1227d37c&pf_rd_r=21W227CVAZJ7J4JQHAHQ&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1588978526&rnid=1266092011&s=electronics&sr=1-1",
+  img:"https://m.media-amazon.com/images/I/71YZlXXFktL._AC_UL480_FMwebp_QL65_.jpg"
+},
+{
+  id: 2,
+  name: '2. Acer Aspire 5',
+  price: 699,
+  description: '5 Slim Laptop, 15.6 inches Full HD IPS Display, AMD Ryzen 3 3200U, Vega 3 Graphics, 4GB DDR4, 128GB SSD, Backlit Keyboard, Windows 10 in S Mode, A515-43-R19L, Silver',
+  rating:'7.3/10',
+  link:'https://www.amazon.com/Acer-Display-Graphics-Keyboard-A515-43-R19L/dp/B07RF1XD36/ref=lp_16225007011_1_6',
+  img:"https://images-na.ssl-images-amazon.com/images/I/41vMYgD92xL.jpg"
+},
+{
+  id: 3,
+  name: '3. Anti-Scratch Shock Absorption Cases',
+  price: 299,
+  description: "Material	TPU, Polycarbonate Brand	Mkeke, Form Factor	Bumper Color	Clear Compatible Phone Models	Apple iPhone XR",
+  rating:"6.5/10",
+  link:"https://www.amazon.com/Compatible-iPhone-Clear-Anti-Scratch-Absorption/dp/B07HRJL27Z/?_encoding=UTF8&pd_rd_w=03g48&pf_rd_p=61a95fae-e859-4f27-858a-6146c1ae863a&pf_rd_r=BXPN2KB49B786MYNX5KV&pd_rd_r=238ea2ce-6ad0-468f-a6c4-e320dd8fa9fe&pd_rd_wg=fN0RD&ref_=pd_gw_unk",
+  img:"https://m.media-amazon.com/images/I/41WOalZ1N5L._AC_SY200_.jpg"
+},
+{
+  id: 4,
+  name: '4. NVIDIA Jetson AGX Xavier Developer Kit (32GB)',
+  price: 495,
+  description: 'NVIDIA Jetson AGX Xavier Developer Kit (32GB)',
+  rating:"4.5/10",
+  link:"https://www.amazon.com/NVIDIA-Jetson-Xavier-Developer-32GB/dp/B083ZL3X5B/ref=sr_1_2?dchild=1&fst=as%3Aoff&pd_rd_r=238ea2ce-6ad0-468f-a6c4-e320dd8fa9fe&pd_rd_w=4xZW0&pd_rd_wg=fN0RD&pf_rd_p=83ab1c34-7657-4d09-b72d-0a3e4b634b1d&pf_rd_r=BXPN2KB49B786MYNX5KV&qid=1602294815&rnid=16225007011&s=computers-intl-ship&sr=1-2",
+  img:"https://m.media-amazon.com/images/I/61m739zmagL._AC_UL480_FMwebp_QL65_.jpg"
+},
+{
+  id: 5,
+  name: '5. HP EliteBook 840 ',
+  price: 1499,
+  rating:"9.6/10",
+  description: 'G6 14" Notebook - Core i7 i7-8565U - 16 GB RAM - 512 GB SSD - Windows 10 Pro - in-Plane Switching (IPS) Technology - English Keyboard - Intel Optane Memory Ready - 17.25 Hour Bat',
+  link:"https://www.amazon.com/HP-EliteBook-840-Notebook-Plane/dp/B07VBHBRM1/ref=sr_1_14?dchild=1&fst=as%3Aoff&pd_rd_r=238ea2ce-6ad0-468f-a6c4-e320dd8fa9fe&pd_rd_w=4xZW0&pd_rd_wg=fN0RD&pf_rd_p=83ab1c34-7657-4d09-b72d-0a3e4b634b1d&pf_rd_r=BXPN2KB49B786MYNX5KV&qid=1602294815&rnid=16225007011&s=computers-intl-ship&sr=1-14",
+  img:"https://m.media-amazon.com/images/I/811p1jxp2pL._AC_UL480_FMwebp_QL65_.jpg"
+},
+{
+  id: 6,
+  name: '6. Bed',
+  price: 1099,
+  description: 'Linenspa All-Season White Down Alternative Quilted Comforter - Corner Duvet Tabs - Hypoallergenic - Plush Microfiber Fill - Machine Washable - Duvet Insert or Stand-Alone Comforter - Queen',
+  rating:"9.9/10",
+  link:"https://www.amazon.com/LINENSPA-All-Season-Alternative-Quilted-Comforter/dp/B00VGR4VW4/ref=sr_1_1?dchild=1&keywords=Home+bedding&pd_rd_r=238ea2ce-6ad0-468f-a6c4-e320dd8fa9fe&pd_rd_w=6YHYd&pd_rd_wg=fN0RD&pf_rd_p=bbbef69f-3d9e-41ab-b1eb-e08d5690939b&pf_rd_r=BXPN2KB49B786MYNX5KV&qid=1614100780&s=kitchen-intl-ship&sr=1-10",
+  img:"https://m.media-amazon.com/images/I/71uR6m9TrIL._AC_UL480_FMwebp_QL65_.jpg"
+},
+{
+  id: 7,
+  name: '7. Lamp',
+  price: 21,
+  description: 'Colors That Energize: The LED strip lights are packed with traditional RGB and 16 other colors. ',
+  rating:"7.6/10",
+  link:"https://www.amazon.com/Govee-Colored-Cupboard-Lighting-Adhesive/dp/B07RX57HDL/ref=sr_1_1?dchild=1&keywords=strip+lighting&pd_rd_r=238ea2ce-6ad0-468f-a6c4-e320dd8fa9fe&pd_rd_w=5EQFG&pd_rd_wg=fN0RD&pf_rd_p=7fefe83c-4f3e-40ee-bbb8-70a26b751735&pf_rd_r=BXPN2KB49B786MYNX5KV&qid=1614100926&sr=8-1",
+  img:"https://m.media-amazon.com/images/I/61A74GGbj5L._AC_UL480_FMwebp_QL65_.jpg"
+},
+{
+  id: 8,
+  name: '8. Oculus',
+  price: 10299,
+  description: 'NVIDIA GTX 1050Ti/AMD Radeon RX 470 or greater Graphics Card Required and a RAM of 8 GB+ RAM',
+  rating:"8.4/10",
+  link:"https://www.amazon.com/Oculus-Touch-Virtual-Reality-System-pc/dp/B073X8N1YW/ref=pd_sbs_4?pd_rd_w=ruafO&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=J86PRJK6X81E44GD2P90&pd_rd_r=f7f0ebe2-f5b3-4ab6-98ea-8b27c918f661&pd_rd_wg=Kt3q0&pd_rd_i=B073X8N1YW&psc=1",
+  img:"https://images-na.ssl-images-amazon.com/images/I/61ueGFutGgL._AC_UL160_SR160,160_.jpg"
+},
+{
+  id: 9,
+  name: '9. Fitbit',
+  price: 299,
+  description: 'Fitbit Charge 4 Fitness and Activity Tracker with Built-in GPS, Heart Rate, Sleep & Swim',
+  rating:"9.5/10",
+  link:"https://www.amazon.com/Fitbit-Fitness-Activity-Tracking-Included/dp/B084CQ41M2/ref=sr_1_10?dchild=1&fst=as%3Aoff&pf_rd_i=16225014011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a3460e00-9eac-4cab-9814-093998a3f6d8&pf_rd_r=REFC7ENHKVRAY7MRPQ3J&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1489015525&rnid=16225014011&s=sporting-goods&sr=1-10",
+  img:"https://m.media-amazon.com/images/I/71smqRr0pmL._AC_UL480_FMwebp_QL65_.jpg"
+},
+{
+  id: 10,
+  name: '10. Ab Roller for Abs Workout',
+  price: 99,
+  description: 'Fitnessery ab roller acts as your personal fitness trainer - helping you to build stronger and bigger six pack abs, burn calories, build muscle and improve your overall endurance. Your ab roller wheel comes with 3 free bonus gifts and life-time money back guarantee. ',
+  rating:"6.3/10",
+  link:"https://www.amazon.com/Ab-Machine-Exercise-Equipment-Workout/dp/B00ZPAWV5C/ref=sr_1_13?dchild=1&fst=as%3Aoff&pf_rd_i=16225014011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a3460e00-9eac-4cab-9814-093998a3f6d8&pf_rd_r=REFC7ENHKVRAY7MRPQ3J&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1489015525&rnid=16225014011&s=sporting-goods&sr=1-13",
+  img:"https://m.media-amazon.com/images/I/91xVOnMVbfL._AC_UL480_FMwebp_QL65_.jpg"
+},
+];
